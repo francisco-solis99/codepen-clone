826e003b024391f61ef9c36f8350c22554db753b
francisco-solis99 <franciscosoliscss@gmail.com>
2022-10-20 12:52:00 -0500

    [Add] some logic to open some buttons

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 import './styles/style.scss';
+
+const buttonOpts = document.querySelector('.button__preview');
+const headerOptions = document.querySelector('.options');
+
+const buttonTools = document.querySelector('.button__tools');
+const tools = document.querySelector('.tools');
+
+buttonOpts.addEventListener('click', () => {
+  headerOptions.classList.toggle('active');
+  buttonOpts.classList.toggle('rotate');
+})
+buttonTools.addEventListener('click', () => {
+  buttonTools.classList.toggle('rotate');
+  tools.classList.toggle('active');
+});
