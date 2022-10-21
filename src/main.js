85f6d2ad581d80547728e8fb3c1ba718a4dcee7f
francisco-solis99 <franciscosoliscss@gmail.com>
2022-10-21 15:28:41 -0500

    [Add] the code and preview section in the codpeen clone and some logic for the buttons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './styles/style.scss';
 
+// Header buttons
 const buttonOpts = document.querySelector('.button__preview');
 const headerOptions = document.querySelector('.options');
 
@@ -14,3 +15,41 @@ buttonTools.addEventListener('click', () => {
   buttonTools.classList.toggle('rotate');
   tools.classList.toggle('active');
 });
+
+
+// Code editors buttons
+// console.log(screen.width);
+const codeHeader = document.querySelector('.code__header');
+const buttonsCode = document.querySelectorAll('.code__header .code__title');
+const spaces = document.querySelectorAll('.code__tech');
+
+const buttonsSpaces = Array.from(buttonsCode).map((btn, index) => {
+  return {
+    button: btn,
+    space: spaces[index]
+  };
+});
+
+buttonsCode.forEach((btn, index) => {
+  btn.addEventListener('click', () => toggleCodeSpace(index, buttonsSpaces.length));
+});
+
+function toggleCodeSpace(index, options) {
+  for (let i = 0; i < options; i += 1) {
+    const { button, space } = buttonsSpaces[i]
+    if (i !== index) {
+      button.classList.remove('active');
+      space.classList.remove('active');
+      continue;
+    }
+    button.classList.toggle('active');
+    space.classList.toggle('active');
+  }
+
+  if (buttonsSpaces[index].space.classList.contains('active')) {
+    codeHeader.classList.add('active');
+    return;
+  }
+  codeHeader.classList.remove('active');
+
+}
