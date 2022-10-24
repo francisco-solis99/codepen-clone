af7ac978b4e216f9fdf33c743c53ccd8f36e54c7
francisco-solis99 <franciscosoliscss@gmail.com>
2022-10-24 15:13:22 -0500

    [Add] the lines and the code area to the editor

--- src/main.js
+++ src/main.js
@@ -53,3 +53,60 @@ function toggleCodeSpace(index, options) {
   codeHeader.classList.remove('active');
 
 }
+
+
+// =========================================== Code Editor ========================================
+const langs = ['html', 'css', 'js'];
+const editors = document.querySelectorAll('.editor__area');
+const gutters = document.querySelectorAll('.editor__gutter');
+
+const editorsGutterPerLang = Array.from(editors).map((editor, index) => [[langs[index]], [editor, gutters[index]]]);
+const structureLangs = Object.fromEntries(editorsGutterPerLang);
+
+
+editors.forEach((editor) => editor.addEventListener('keyup', (e) => manageGutter(e)));
+
+let counterLines = 1;
+
+function manageGutter(e) {
+  const { key, target } = e;
+  if (!['Enter', 'Backspace'].includes(key)) return;
+
+  const editorName = target.dataset.lang;
+  const editorToManage = structureLangs[editorName];
+  const [, gutter] = editorToManage;
+
+
+  if (key === 'Enter') {
+    addLineFromGutter(gutter)
+    return;
+  }
+
+  if (key === 'Backspace') {
+    const editorCurrentLines = target.value.split('\n').length;
+    if (editorCurrentLines === counterLines) return;
+    removeLineFromGutter(gutter, editorCurrentLines);
+  }
+
+}
+
+function removeLineFromGutter(editorGutter, currentLines) {
+  while (counterLines !== currentLines) {
+    counterLines -= 1;
+    const spanLine = editorGutter.querySelector('span:last-child');
+    spanLine.remove();
+  }
+}
+
+function addLineFromGutter(editorGutter) {
+  counterLines += 1;
+  const newSpanLine = createEditorLine();
+  editorGutter.appendChild(newSpanLine);
+}
+
+
+function createEditorLine() {
+  const span = document.createElement('span');
+  span.textContent = counterLines;
+  return span;
+}
