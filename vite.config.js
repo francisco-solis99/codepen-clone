2595e10b9b0eb375fbdaff1537138aebfed85396
francisco-solis99 <franciscosoliscss@gmail.com>
2022-11-05 22:25:38 -0600

    [Add] the github action to deploy with vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
-  base: 'codepen-clone',
+  base: '/codepen-clone/',
   publicDir: '../public',
   build: {
     outDir: '../dist',
